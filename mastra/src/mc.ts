13ddce463530ecf04e8b5d0d24c861f211a50f02
erik-dunteman <erikdunteman@gmail.com>
2025-08-29 12:44:14 -0700

    adds docker server

---
+++ mastra/src/mc.ts
@@ -0,0 +1,8 @@
+const mineflayer = require("mineflayer");
+
+const options = {
+  host: "localhost", // Change this to the ip you want.
+  port: 19132, // Change this to the port you want.
+};
+
+const bot = mineflayer.createBot(options);
