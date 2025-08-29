46b0dd493fa8182290d9baa9448a1d8be113efd9
erik-dunteman <erikdunteman@gmail.com>
2025-08-29 13:29:45 -0700

    fix offline mode

--- mastra/src/flayer.ts
+++ mastra/src/flayer.ts
@@ -1,8 +1,11 @@
 import mineflayer from "mineflayer";
+import { mineflayer as mineflayerViewer } from "prismarine-viewer";
 
 const options = {
   host: "localhost", // Change this to the ip you want.
   port: 25565, // Change this to the port you want.
+  username: "Bot",
+  auth: "offline",
 };
 
 const bot = mineflayer.createBot(options);
@@ -13,6 +16,7 @@ bot.once("login", () => {
 
 bot.once("spawn", () => {
   console.log("[mc] Spawned into the world");
+  // mineflayerViewer(bot, { port: 3000, firstPerson: false });
 });
 
 bot.on("kicked", (reason, loggedIn) => {
