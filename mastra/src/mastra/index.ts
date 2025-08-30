ca1fc9a60fe75eae1578c3bc85e234fe717b3f83
erik-dunteman <erikdunteman@gmail.com>
2025-08-30 12:53:12 -0700

    remove mastra examples

--- mastra/src/mastra/index.ts
+++ mastra/src/mastra/index.ts
@@ -1,22 +1,13 @@
 import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
 import { LibSQLStore } from "@mastra/libsql";
-import { weatherWorkflow } from "./workflows/weather-workflow";
-import { testWorkflow } from "./workflows/test-workflow";
-import { weatherAgent } from "./agents/weather-agent";
 import { minecraftAgent } from "./agents/minecraft-agent";
-import { planRunWorkflow, singlePlanRunWorkflow } from "./workflows/plan-run";
 import { plannerAgent } from "./agents/planner";
 import { workerAgent } from "./agents/worker";
 
 export const mastra = new Mastra({
-  workflows: {
-    weatherWorkflow,
-    testWorkflow,
-    planRunWorkflow,
-    singlePlanRunWorkflow,
-  },
-  agents: { weatherAgent, minecraftAgent, plannerAgent, workerAgent },
+  workflows: {},
+  agents: { minecraftAgent, plannerAgent, workerAgent },
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
     url: ":memory:",
